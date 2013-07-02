fd8ee9ec1499895593e1b848d807a7cc399ae106
shimion <shimion_b@yahoo.com>
2013-07-03 05:11:30 +0600

    Testing
    This is for testing perpose

---
+++ footer-page.php
@@ -0,0 +1,68 @@
+<div class="container">
+  		<div class="row main" style="margin-top:0px; padding-top:0px;">
+			<div class="space_40" style="margin-right:23px;">
+                
+                
+                
+                    
+                <div class="clear"></div>    
+                  <div class="twelve columns policy-area" style="margin-top:10px;">
+                  
+                    	<div class="social_icon">
+                      	<img src="<?php echo get_template_directory_uri(); ?>/images/with_contact_us.png" />
+                    	<a href="<?php echo home_url(); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/in.png" /></a>
+                        
+                        
+                        
+                        <a href="<?php echo home_url(); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/you_tube.png" /></a>
+                        
+                        
+                        <a href="<?php echo home_url(); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter.png" /></a>
+                        
+                         <a href="<?php echo home_url(); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/facebook.png" /></a>
+                         
+                         
+                          <a href="<?php echo home_url(); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/google.png" /></a>
+                          
+                          
+                          <a href="<?php echo home_url(); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/eee.png" /></a>
+                          
+                          
+                    </div>
+                  </div>
+                    
+                    
+                </div>
+			</div>
+		</div> 
+        
+
+<div class="container">
+	<div class="row">
+    	<img src="<?php echo get_template_directory_uri(); ?>/images/footer-shedow.png" />
+    </div>
+</div>        
+        
+        
+        
+        
+        
+        
+           
+<!--</div>-->
+
+
+	  
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-1719540-15', 'blinkhq.com');
+  ga('send', 'pageview');
+
+</script>
+<?php wp_footer(); ?>
+</body>
+</html>
